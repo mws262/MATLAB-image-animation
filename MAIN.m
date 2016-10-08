6871a1f909a48eeaccb7dd8be31c0f2015540c85
Matt Sheen <mws262@cornell.edu>
2016-10-08 13:34:33 -0400

    updated full animation in MAIN

--- MAIN.m
+++ MAIN.m
@@ -1,6 +1,6 @@
 %% Example use - Perspective video rendering
 %  For making little animations in which images can move around in
-%  perspective. 
+%  perspective.
 %
 %  Matthew Sheen
 
@@ -10,15 +10,40 @@ close all; clear all;
 fig = figure;
 hold on
 fig.Color = [1,1,1];
-fig.Position = [100,100,960,540];
+fig.Position = [100,100,1920*3/4,1080*3/4];
 
 %% Add surfaces
 % Import images, skin them to surfaces.
 [keySurf,keySurfTrans] = setUpImage('full_keyboard.png',0.2);
+[QSurf,QSurfTrans] = setUpImage('Q.png',0.2);
+[WSurf,WSurfTrans] = setUpImage('W.png',0.2);
 [OSurf,OSurfTrans] = setUpImage('O.png',0.2);
+[PSurf,PSurfTrans] = setUpImage('P.png',0.2);
+
+[BSurf,BSurfTrans] = setUpImage('browser.png',1);
 
 % Change initial transforms from being solely centered around the origin.
-OSurfTrans.Matrix = makehgtform('translate',0,0,1);
+QSurfTrans.Matrix = makehgtform('translate',-410,79,1);
+WSurfTrans.Matrix = makehgtform('translate',-330,79,1);
+OSurfTrans.Matrix = makehgtform('translate',380,79,1);
+PSurfTrans.Matrix = makehgtform('translate',460,79,1);
+BSurfTrans.Matrix = makehgtform('translate',0,400,450,'xrotate',pi/2);
+
+% Keey track of the original transformations
+QTrans0 = QSurfTrans.Matrix;
+WTrans0 = WSurfTrans.Matrix;
+OTrans0 = OSurfTrans.Matrix;
+PTrans0 = PSurfTrans.Matrix;
+BTrans0 = BSurfTrans.Matrix;
+
+
+% Keep track of the alpha channel data
+keyAlpha = keySurf.AlphaData;
+QAlpha = QSurf.AlphaData;
+WAlpha = WSurf.AlphaData;
+OAlpha = OSurf.AlphaData;
+PAlpha = PSurf.AlphaData;
+BAlpha = BSurf.AlphaData;
 
 %% Set up axes
 ax = fig.Children;
@@ -30,6 +55,8 @@ axis([ax.XLim, ax.YLim]);
 %% Initial camera settings
 ax.CameraViewAngle = 40;
 initTarget = ax.CameraTarget;
+ax.CameraPosition = [1000,-10*200 + initTarget(2),2000];
+ax.CameraUpVector = [0,0,1];
 ax.Projection = 'perspective';
 
 
@@ -37,23 +64,107 @@ ax.Projection = 'perspective';
 vw = VideoWriter('test','MPEG-4');
 open(vw); % Remember to call close after the animation.
 
+fps = 10;
+tstep = 1/fps;
+tFinal = 12;
+lastTrans = [1 1 1 1 1 1]; % Keep track of the last set of transparency values so we don't go around changing them all the time.
 
 %% Put loops below to move camera, do transforms, etc.
 % Example camera move
-for i = 1:-0.01:0
-    ax.CameraUpVector = [0,1,0];
-    ax.CameraPosition = [initTarget(1),i*200 + initTarget(2),600];
+for time = 9:tstep:tFinal  
+%% Camera
+    % Camera positions, targets, up-vectors are interpolated in time to get
+    % nice smooth transitions.    
+    camTimings = [0; 2; % Initial view
+        3; 5;
+        7; 10;
+        11.5; tFinal];
     
-    drawnow;
-    fr = getframe(gcf);
-    writeVideo(vw,fr);
-end
+    camTargets = [0 400 445; 0 400 445; 
+        0 200 200; 0 200 200;
+        0 0 0; 0 0 0;
+        0 0 0; 0 0 0];
+    
+    camPositions = [0 -480 445; 0 -480 445;
+        800 -1500 1000; 800 -1500 1000;
+        100 -100 1200; 100 -100 1200;
+        0 -100 1200; 0 -100 1200];
+    
+    camUps = [0 0 1; 0 0 1;
+        0 0 1; 0 0 1;
+        0 1 0; 0 1 0;
+        0 1 0; 0 1 0];
+      
+    % Camera stuff - Do camera interpolation and assign to this axis.
+    campos(interp1(camTimings,camPositions,time,'linear'));
+    camtarget(interp1(camTimings,camTargets,time,'linear'));
+    camup(interp1(camTimings,camUps,time,'linear'));
+    
+%% Transparencies
+    transTimings = [0;6
+        7;10;
+        tFinal-0.1;tFinal];
+    % [Key surface - Q - W - O - P - Browser] is the order
+    transparencies = [1,1,1,1,1,1; 1,1,1,1,1,1;
+    0.5,1,1,1,1,0.2; 0.5,1,1,1,1,0.2;
+    0.1,1,1,1,1,0.; 0.1,1,1,1,1,0.];
 
-% Example image move
-for i = 0:0.05:10
-    OSurfTrans.Matrix = makehgtform('scale',1.01)*OSurfTrans.Matrix;
+    currTrans = interp1(transTimings,transparencies,time,'linear');
+
+    changeTrans = ~(currTrans == lastTrans); % Only change transparencies when something has changed. It's super slow since it's a full map the same size as the image.
+
+    if changeTrans(1), keySurf.AlphaData = keyAlpha*currTrans(1); end
+    if changeTrans(2), QSurf.AlphaData = QAlpha*currTrans(2); end
+    if changeTrans(3), WSurf.AlphaData = WAlpha*currTrans(3); end
+    if changeTrans(4), OSurf.AlphaData = OAlpha*currTrans(4); end
+    if changeTrans(5), PSurf.AlphaData = PAlpha*currTrans(5); end
+    if changeTrans(6), BSurf.AlphaData = BAlpha*currTrans(6); end
     
+    lastTrans = currTrans;
+    
+%% Movements/transforms
+
+    stM = 7.3;
+    
+    moveTimings = [0;stM;
+                   stM + 0.5; stM + 0.6;
+                   stM + 1.1; stM + 1.2;
+                   stM + 1.7; stM + 1.8;
+                   stM + 2.3; stM + 2.4;
+                   stM + 2.9; stM + 3.0;
+                   stM + 3.5; stM + 3.6;
+                   stM + 4.5; tFinal];
+               
+    QTranslate = [0 0 0; 0 0 0;
+                   0 0 30; 0 0 30;
+                   0 0 0;  0 0 0;
+                   0 0 30; 0 0 30;
+                   0 0 0;  0 0 0;
+                   0 0 30; 0 0 30;
+                   0 0 0;  0 0 0;
+                   315 0 250; 315 0 250];
+               
+    OTranslate = [0 0 0; 0 0 0;
+                   0 0 30; 0 0 30;
+                   0 0 0;  0 0 0;
+                   0 0 30; 0 0 30;
+                   0 0 0;  0 0 0;
+                   0 0 30; 0 0 30;
+                   0 0 0;  0 0 0;
+                   -315 0 250; -315 0 250];
+               
+    currQmove = interp1(moveTimings,QTranslate,time,'linear');
+    currOmove = interp1(moveTimings,OTranslate,time,'linear');
+        
+    QSurfTrans.Matrix = makehgtform('translate',currQmove)*QTrans0;
+    WSurfTrans.Matrix = makehgtform('translate',currQmove)*WTrans0;
+    OSurfTrans.Matrix = makehgtform('translate',currOmove)*OTrans0;
+    PSurfTrans.Matrix = makehgtform('translate',currOmove)*PTrans0;
+
+    
+%% Draw and record frame
     drawnow;
+    
     fr = getframe(gcf);
     writeVideo(vw,fr);
 end
